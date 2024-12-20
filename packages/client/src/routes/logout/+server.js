8e8f081551b7ac2916d0cae5c4329ab2d04617cd
Dimitrij Agal <dimitrij.agal@gmail.com>
2024-12-20 14:53:44 +0700

    delete cookie not working in virtual host

--- packages/client/src/routes/logout/+server.js
+++ packages/client/src/routes/logout/+server.js
@@ -1,9 +1,15 @@
 import { COOKIE_ACCESS_TOKEN } from '$lib/constants.js';
+import { NODE_ENV } from '$env/static/private';
 import serverFetch from '$lib/fetcher/fetch.js';
 import { redirect } from '@sveltejs/kit';
 
 export const GET = async ({ cookies }) => {
 	await serverFetch('/user/signout', {}, cookies.get(COOKIE_ACCESS_TOKEN));
-	cookies.delete(COOKIE_ACCESS_TOKEN, { path: '/' });
+	cookies.delete(COOKIE_ACCESS_TOKEN, {
+		path: '/',
+		httpOnly: true,
+		secure: NODE_ENV === 'production' ? true : false,
+		sameSite: 'strict'
+	});
 	return redirect(303, '/login');
 };
